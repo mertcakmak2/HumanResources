640de8f027c31a5c724ca55b15e6c2ffca399537
mertcakmak2 <mertcakmak2@gmail.com>
2021-07-23 14:03:48 +0300

    Cors error fixed from axios request

--- src/main/java/com/company/hrms/Config/WebSecurityConfig.java
+++ src/main/java/com/company/hrms/Config/WebSecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -44,7 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
+        //http.csrf().disable().authorizeRequests()
                 .antMatchers("/api/auth/login").permitAll()
                 .antMatchers("/api/register/job-seeker").permitAll()
                 .antMatchers("/api/register/job-seeker/confirm").permitAll()
--- src/main/java/com/company/hrms/HrmsApplication.java
+++ src/main/java/com/company/hrms/HrmsApplication.java
@@ -1,12 +1,10 @@
 package com.company.hrms;
 
-import com.cloudinary.Cloudinary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HrmsApplication {
@@ -14,5 +12,17 @@ public class HrmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(HrmsApplication.class, args);
     }
+
+    /*@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST",
+                                "DELETE", "PATCH").allowedHeaders("*");
+            }
+        };
+    }*/
 
 }
